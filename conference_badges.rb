187191460c582472be6b16044721ec41ae86ba38
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-09-16 19:13:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(people)
+ people.map do |person|
+   badge_maker(person)
+  end
+end
+
+def assign_rooms(people)
+  people.map.with_index(1) do |person, index|
+    "Hello, #{person}! You'll be assigned to room #{index}!"
+  end
+end
+
+def printer(people)
+  people.map do 
+    puts 
+end
+end
